8b51cef83d64d3917e4356563240258a63a9d4f0
axxo- <axxo->
2004-01-27 21:35:09 +0000

    Heb er een class van gemaakt, lijkt me beter ook de source in 1 taal gezet, was engels/nederlands dooreen, meeste was engels die dus genomen gettext geprobeert, snap het niet volledig, maar lijkt te werken (LANG=nl ./pytele... LANG=en ./pytele...) Mijn spelling/taal skills zijn onbestaande, dus die moeten gefixt worden

--- lib/pytelemeter.py
+++ lib/pytelemeter.py
@@ -2,7 +2,7 @@
 #
 # Geeft download en upload statistieken van de Mijn Telenet pagina.
 # door Robbie Vanbrabant <robbie.vanbrabant@pandora.be>
-# alot of changes by Thomas M <knu@keanu.be>  ;-)
+# en Thomas Matthijs <knu@keanu.be>  ;-)
 
 import sys
 import ClientCookie
@@ -10,139 +10,139 @@ import ConfigParser
 import os
 import re
 
+#gettext
+import gettext
+t = gettext.translation('pytelemeter', '/usr/share/locale')
+_ = t.ugettext
+
 VERSION = "0.8"
 
 # configuratiebestand
 configfile = os.environ['HOME'] + "/.pytelemeterrc"
+
+
+URL_FIRST = "https://www.telenet.be/sys/sso/exec_login.php"
+URL_FIRST_DATA = "uid=%s&pwd=%s&goto=http%%3A%%2F%%2Fwww.telenet.be"
+URL_MAIN = "https://www.telenet.be/sys/sso/jump.php?https://services.telenet.be/isps/MainServlet?ACTION=TELEMTR&SSOSID=%s"
+URL_OVERVIEW = "https://services.telenet.be/isps/be/telenet/ebiz/ium/Histogram.jsp" 
+
+
+REGEX_FIRST_SSOSID = "<!-- SSOSID => ([^ ]+) -->"
+
+
+REGEX_OVERVIEW_TOTALUSED = "(?s)<(?:TD class=\"header\" align=\"right\"*)>(.*?) MB</TD>"
+REGEX_OVERVIEW_DAYS = "<TR>[^<]+<TD class=\"(?:odd|even)\">[^0-9]+([0-9]{2}/[0-9]{2}/[0-9]{2})[^<]+</TD>[^<]+<TD class=\"(?:odd|even)\" align=\"right\">[^0-9]+([0-9]+)[^<]+</TD>[^<]+<TD class=\"(?:odd|even)\" align=\"right\">[^0-9]+([0-9]+)[^<]+</TD>[^<]+</TR>"
+REGEX_MAIN_USED_TOTAL = "Totaal verbruikt volume \(downstream \+ upstream\)</a>[^<]+<b>([0-9]+)%</b><br>"
+REGEX_MAIN_USED_UPLOAD = "Upstream verbruikt volume\</a>[^<]+<b>([0-9]+)%</b><br>"
+REGEX_MAIN_MAX = "Het toegelaten totaal volume op jouw product is[^<]+<b>[ ]*([0-9]+)[ ]*GB</b> per 30 dagen, waarvan[^<]+<b>[ ]*([,0-9]+)[ ]*GB</b> upstream"
+REGEX_MAIN_DATEBROAD = "Door overschrijding van uw toegelaten volume werd de snelheid van uw modem beperkt. Uw snelheid wordt automatisch hersteld op <b>[^0-9]+([0-9]{2}/[0-9]{2}/[0-9]{4})[^<]+</b>"
 
 def fatalError(errormsg):
 	print errormsg
 	sys.exit(1)
 
 
-def checkConfig():
-	if os.path.isfile(configfile):
-		mode = os.stat(configfile).st_mode
-        	if oct(mode & 0777) <> oct(0600 & 0777):
-			fatalError(	"\nHet configuratiebestand MOET chmod 0600 staan om veiligheidsredenen\n" +
-					"Huidige permissies: " + oct(mode & 0777) +  "\n" +
-					"\nVoer bijvoorbeeld uit: chmod 600 ~/.pytelemeterrc\n")
-	else:
-		fatalError("\nHet configuratiebestand is niet gevonden\n")
+class Telemeter:
+	def __init__(self):
+		self.username = ""
+		self.password = ""
 
-def getConfig():
-	global user
-	global passwd
-	try:
-		config = ConfigParser.ConfigParser()
-		config.read(configfile)
-		user= config.get("user","user")
-		passwd= config.get("user","passwd")
-	except ConfigParser.NoSectionError:
-		fatalError("\nKijk of de nodige secties bestaan in het configuratiebestand.\n")
-	except ConfigParser.DuplicateSectionError:
-		fatalErro("\nTwee dezelfde configuratiesecties gevonden\n")
-	except ConfigParser.NoOptionError:
-		fatalErro("\nNiet voldoende configuratieopties gevonden\n")
-	except ConfigParser.MissingSectionHeaderError:
-		fatalError("\nGeen configuratiesecties in het bestand\n")
-	except ConfigParser.ParsingError:
-		fatalError("\nKan configbestand niet parsen\n")
-	except:
-		fatalError("\nUnexpected error:", sys.exc_info()[0])
-		#wat doet raise?
-		#If no expressions are present, raise re-raises the last expression that was active in the current scope
-		#allowing a caller to handle the exception as well
-		raise
-	if user == 'foo' or passwd == 'bar':
-		fatalError("\nEditeer het configuratiebestand eerst!\n")
-
-#def setDebug():
-	# TODO: minder lelijk uitwerken
-	#print "Python %s" % sys.version[0:5]
-	#print "ClientCookie %s\n" % ClientCookie.VERSION
-
-	# debug mode
-	#hh = ClientCookie.HTTPHandler()
-	#sh = ClientCookie.HTTPSHandler()
-	#for h in [hh, sh]:
-	#    h.set_http_debuglevel(1)
-	#ClientCookie.install_opener(ClientCookie.build_opener(hh, sh))
-
-def getInfo():
-	global htmlMain
-	global htmlOverzicht
-	cookieUrl = "https://www.telenet.be/sys/sso/exec_login.php"
-	goto = "http%3A%2F%2Fwww.telenet.be"
-
-	# openen van telenet website voor het verkrijgen van een sessie id
-	try:
-		data = "uid=" + user + "&pwd=" + passwd + "&goto=" + goto
-		r = ClientCookie.urlopen(cookieUrl, data)
-		output = r.read()
-	except IOError, e:
-		fatalError("Fout bij het verkrijgen van een SSOSID: %s" %e)
-        
-	# het SSOSID 
-        SSOSID = re.search("<!-- SSOSID => ([^ ]+) -->",output).group(1)
-	
-	# telemeter pagina openen
-	try:
-		meterurl = 'https://www.telenet.be/sys/sso/jump.php?https://services.telenet.be/isps/MainServlet?ACTION=TELEMTR&SSOSID=' + SSOSID
-		pagina = ClientCookie.urlopen(meterurl)
-		htmlMain = pagina.read()
-	except IOError, e:
-		fatalError("Fout bij het openen van de Telemeter pagina: %s" %e)
-
-	# gedetailleerde info opvragen
-	try:
-		detail = ClientCookie.urlopen("https://services.telenet.be/isps/be/telenet/ebiz/ium/Histogram.jsp")
-		htmlOverzicht = detail.read()
-	except IOError, e:
-		fatalError("Fout bij het openen van de dagoverzicht pagina: %s" %e)
+		self.htmlMain = ""
+		self.htmlOverview = ""
 		
-def getVolumeUsed():
-	#Totaal verbruikt,upload verbuikt in MiB
-	regex = re.compile(r'<(?:TD class="header" align="right"*)>(.*?) MB</TD>', re.M)
-	match = regex.findall(htmlOverzicht)
+	def fetch(self):
+		if (self.username == "" or self.password == ""):
+			self.checkConfig()
+			self.getConfig()
+		# "Fetching information" in nederlands? kan op niets beter komen :p
+		sys.stdout.write(_("Information aan het grijpen... "))
+		sys.stdout.flush()
+		 
+		# lets get a cookie first, so hungry
+		htmlFirst = self.getHtml(URL_FIRST, URL_FIRST_DATA % (self.username, self.password))
+		# we also need the SSOSID  from that page
+		ssosid = re.search(REGEX_FIRST_SSOSID, htmlFirst).group(1)
+		# get the main page
+		self.htmlMain = self.getHtml(URL_MAIN % ssosid)
+		# get the overview page
+		self.htmlOverview = self.getHtml(URL_OVERVIEW)
+		
+		print _("done!\n")
+		
+	def checkConfig(self):
+		if os.path.isfile(configfile):
+			mode = os.stat(configfile).st_mode
+			if oct(mode & 0777) <> oct(0600 & 0777):
+				fatalError(	_("\nHet configuratiebestand MOET chmod 0600 staan om veiligheidsredenen\n") +
+						_("Huidige permissies: ") + oct(mode & 0777) +  "\n" +
+						_("\nVoer bijvoorbeeld uit: chmod 600 ~/.pytelemeterrc\n"))
+		else:
+			fatalError(_("\nHet configuratiebestand is niet gevonden\n"))
 
-	totmb = int(match[0])
-	upmb = int(match[1])
-	return totmb,upmb
-	
-def getOverzicht():
-	# Overzicht
-	dagOverzicht = re.findall('<TR>[^<]+<TD class="(?:odd|even)">[^0-9]+([0-9]{2}/[0-9]{2}/[0-9]{2})[^<]+</TD>[^<]+<TD class="(?:odd|even)" align="right">[^0-9]+([0-9]+)[^<]+</TD>[^<]+<TD class="(?:odd|even)" align="right">[^0-9]+([0-9]+)[^<]+</TD>[^<]+</TR>',htmlOverzicht)
-	return dagOverzicht
-	
-def getVolumeUsedProcent():
-	# % verbruikt
- 	volumeTotaal = re.search('Totaal verbruikt volume \(downstream \+ upstream\)</a>[^<]+<b>([0-9]+)%</b><br>',htmlMain).group(1)
-	volumeUpload = re.search('Upstream verbruikt volume\</a>[^<]+<b>([0-9]+)%</b><br>',htmlMain).group(1)
-	return int(volumeTotaal),int(volumeUpload)
-	
-def getMaxVolume():
-	# zoek maximum volume dat toegestaan is
-	# deze regex werkt voor mij, zou bij iedereen moeten werken, maar staat paar rare spacies in, die van plaats en aantal bljiken te verandere soms,daarom een aantal [ ]*
-	volumeMax = re.search('Het toegelaten totaal volume op jouw product is[^<]+<b>[ ]*([0-9]+)[ ]*GB</b> per 30 dagen, waarvan[^<]+<b>[ ]*([,0-9]+)[ ]*GB</b> upstream',htmlMain)
-	volumeDownMax = int(volumeMax.group(1))
-	volumeUpMax = float(volumeMax.group(2).replace(",","."))
-	return volumeDownMax,volumeUpMax
+	def getConfig(self):
+		try:
+			config = ConfigParser.ConfigParser()
+			config.read(configfile)
+			self.username = config.get("user", "user")
+			self.password = config.get("user", "passwd")
+		except ConfigParser.NoSectionError:
+			fatalError(_("\nKijk of de nodige secties bestaan in het configuratiebestand.\n"))
+		except ConfigParser.DuplicateSectionError:
+			fatalError(_("\nTwee dezelfde configuratiesecties gevonden\n"))
+		except ConfigParser.NoOptionError:
+			fatalError(_("\nNiet voldoende configuratieopties gevonden\n"))
+		except ConfigParser.MissingSectionHeaderError:
+			fatalError(_("\nGeen configuratiesecties in het bestand\n"))
+		except ConfigParser.ParsingError:
+			fatalError(_("\nKan configbestand niet parsen\n"))
+		except:
+			fatalError(_("\nUnexpected error:") + sys.exc_info()[0])
+			#If no expressions are present, raise re-raises the last expression that was active in the current scope
+			#allowing a caller to handle the exception as well
+			raise
+		if (self.username == 'foo' or self.password == 'bar'):
+			fatalError(_("\nEditeer het configuratiebestand eerst!\n"))
 
-def getDatumTerugBreedband():
-	#datum dat je terug op breedband komt
-	terugBreed = re.search('Door overschrijding van uw toegelaten volume werd de snelheid van uw modem beperkt. Uw snelheid wordt automatisch hersteld op <b>[^0-9]+([0-9]{2}/[0-9]{2}/[0-9]{4})[^<]+</b>',htmlMain)
-	if terugBreed:
-		return terugBreed.group(1)
-	else:
-		return "Not Found"
+	def getHtml(self, url, data=""):
+		try:
+			if data != "":
+				page = ClientCookie.urlopen(url, data)
+			else: 
+				page = ClientCookie.urlopen(url)
+		except IOError, e:
+			fatalError(_("Fout bij het openen van de Telemeter pagina: %s") %e)
 
-def main():
-	checkConfig()
-	getConfig()
-	#setDebug()
-	getInfo()
+		return page.read()
+
+	def getVolumeUsed(self, procent):
+		if (procent == 1):
+	 		total = int(re.search(REGEX_MAIN_USED_TOTAL, self.htmlMain).group(1))
+			upload = int(re.search(REGEX_MAIN_USED_UPLOAD, self.htmlMain).group(1))
+		else:
+			match = re.findall(REGEX_OVERVIEW_TOTALUSED, self.htmlOverview)
+
+			total = int(match[0])
+			upload = int(match[1])
+		return total, upload
+	
+	def getOverview(self):
+		# Overview
+		return re.findall(REGEX_OVERVIEW_DAYS, self.htmlOverview)
+	
+	def getVolumeMax(self):
+		# allowed volume
+		volumeMax = re.search(REGEX_MAIN_MAX, self.htmlMain)
+		totalMax = int(volumeMax.group(1))
+		uploadMax = float(volumeMax.group(2).replace(",", "."))
+		return totalMax, uploadMax
+
+	def getDateBroad(self):
+		#date you return to broadband
+		date = re.search(REGEX_MAIN_DATEBROAD, self.htmlMain)
+		if date:
+			return date.group(1)
+		else:
+			return _("Niet over limiet")
 
 if __name__ == "__main__":
-	main()
-	sys.exit(0)
+	fatalError(_("Run dit niet rechtstreeks"))
--- setup.py
+++ setup.py
@@ -10,9 +10,11 @@ from distutils.core import setup
 setup(name="pytelemeter",
       version="0.8",
       description="A small Python 'Telemeter' application to check out Telenet's download/upload statistics",
-      author="Robbie Vanbrabant",
+      author="Robbie Vanbrabant, Thomas Matthijs",
       author_email="climaxius@users.sourceforge.net",
       url="http://pytelemeter.sourceforge.net",
+      data_files = [('/usr/share/locale/nl/LC_MESSAGES',['locale/nl/LC_MESSAGES/pytelemeter.mo']),
+      		    ('/usr/share/locale/en/LC_MESSAGES',['locale/en/LC_MESSAGES/pytelemeter.mo'])],
       py_modules=["pytelemeter"],
       scripts=['scripts/pytelemeter-cli', 'scripts/pytelemeter-gtk'],
       package_dir = {'': 'lib'}
