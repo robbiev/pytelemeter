e2adfadb2cb52b1c96c674fcadab7a2fd9cb498a
climaxius <climaxius>
2004-11-30 22:52:45 +0000

    GlobalFunctions added to store -and you will never guess- global funcions :-)

---
+++ pytelemeter/GlobalFunctions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#
+# pytelemeter library 
+# Fetches some statistics from the "Mijn Telenet" page.
+# 
+# See COPYING for info about the license (GNU GPL)
+# Check AUTHORS to see who wrote this software.
+from sys import exit
+
+def fatalError(errormsg):
+        print errormsg
+        exit(1)
+
